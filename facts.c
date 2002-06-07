b2057f50cd963463131da9f8b93e359e52737f72
rmattes <rmattes@720c2900-ecf9-0310-9971-ea17055fec0c>
2002-06-07 11:24:35 +0000

    Fixed numerous namespace issues. Started to clean up code.
    git-svn-id: https://svn.zeit.de/svn/projects/CMS-DBM/trunk@4 720c2900-ecf9-0310-9971-ea17055fec0c

--- facts.c
+++ facts.c
@@ -5,13 +5,6 @@
  *
  */
 
-/* FIXME: We'd like to see an enum here but that would
- * make the interface to SQL harder.
- */
-#define  FACT_ERROR    0
-#define  FACT_CREATED  1
-#define  FACT_UPDATED  2
-#define  FACT_DELETED  3
 
 /* This is void* but we want to distinguish it here */
 typedef void *pg_plan;
@@ -28,19 +21,24 @@ typedef void *pg_plan;
           "UPDATE facts SET value = $4 WHERE " \
           " uri = $1 AND namespace = $2 AND name = $3 "
 
+#define QUERY_FORGET \
+         "DELETE FROM facts WHERE uri = $1 AND namespace = $2 AND name = $3"
+
 #include "executor/spi.h"
 
+/* Function declarations here */
+Datum facts_assert(PG_FUNCTION_ARGS);
+Datum facts_forget(PG_FUNCTION_ARGS);
 
 
-PG_FUNCTION_INFO_V1(assert);
+PG_FUNCTION_INFO_V1(facts_assert);
 Datum
-assert(PG_FUNCTION_ARGS)
+facts_assert(PG_FUNCTION_ARGS)
 {
   Datum           values[4];
   static pg_plan  find_plan, insert_plan, update_plan ;
   static Oid      ctypes[4] = {TEXTOID, TEXTOID, TEXTOID, TEXTOID};
   int             res = -1;
-  int             status;
 
   /* Check for not-NULL */
   if(PG_ARGISNULL(0) || PG_ARGISNULL(1) || PG_ARGISNULL(2) ||PG_ARGISNULL(3)) {
@@ -52,11 +50,9 @@ assert(PG_FUNCTION_ARGS)
   values[2] = PG_GETARG_DATUM(2);
   values[3] = PG_GETARG_DATUM(3);
 
-  status = 0;
   if(SPI_connect() != SPI_OK_CONNECT)
     {
       elog(ERROR, "function facts: SPI_connect failed");
-      return FACT_ERROR;
     }
 
   if(!find_plan)
@@ -96,7 +92,7 @@ assert(PG_FUNCTION_ARGS)
       update_plan = SPI_saveplan(pl);
     }
 
-  /* We have a connection to the server core with our own mamory context
+  /* We have a connection to the server core with our own memory context
    * and a precompiled plan - let's see whether we allready have a similar
    * fact in the databse.
    */
@@ -106,9 +102,6 @@ assert(PG_FUNCTION_ARGS)
     elog(ERROR, "function facts: could not check fact. SPI_exep returned %d", res);
     goto FINISH;
   }
-
-  elog(NOTICE, "QUERY ARGS: %d", fcinfo->nargs);
-
 
   /* Check the number of tuples found */
   switch(SPI_processed) {
@@ -129,5 +122,52 @@ assert(PG_FUNCTION_ARGS)
   
  FINISH:
   SPI_finish();
-  PG_RETURN_INT32(status);
+  PG_RETURN_NULL();
+}
+
+
+PG_FUNCTION_INFO_V1(facts_forget);
+Datum
+facts_forget(PG_FUNCTION_ARGS)
+{
+  Datum           values[3];
+  static pg_plan  delete_plan ;
+  static Oid      ctypes[3] = {TEXTOID, TEXTOID, TEXTOID};
+  int             res = -1;
+
+  if(PG_ARGISNULL(0) || PG_ARGISNULL(1) || PG_ARGISNULL(2)) {
+    elog(ERROR, "function forget(): Can not forget NULL knowlege");
+  }
+
+  values[0] = PG_GETARG_DATUM(0);
+  values[1] = PG_GETARG_DATUM(1);
+  values[2] = PG_GETARG_DATUM(2);
+
+  if(SPI_connect() != SPI_OK_CONNECT)
+    {
+      elog(ERROR, "function facts: SPI_connect failed");
+    }
+
+  if(!delete_plan)
+    {
+      pg_plan pl;
+
+      pl = SPI_prepare(QUERY_FORGET, 4, ctypes);
+      if (pl == NULL) {
+	elog(ERROR, "function facts: SPI_saveplan returned %d", SPI_result);
+	goto FINISH;
+      }
+      elog(NOTICE, "Saved query_delete plan for function assert()");
+      delete_plan = SPI_saveplan(pl);
+    }
+  
+  res = SPI_execp(delete_plan, values, "___", 0);
+  if (res != SPI_OK_DELETE) {
+    elog(ERROR, "function facts: could not forget fact. SPI_exep returned %d", res);
+    goto FINISH;
+  }
+
+ FINISH:
+  SPI_finish();
+  PG_RETURN_NULL();
 }
